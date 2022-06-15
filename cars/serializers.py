34a85146ca0e3b9812b7a8179b3c5d047a2b5a86
unknown <semtehrani@yahoo.com>
2022-06-15 16:28:29 -0700

    Created card Serializer class and file

---
+++ cars/serializers.py
@@ -0,0 +1,10 @@
+# we made this
+
+from rest_framework import serializers   
+from .models import Car
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id','make','model', 'year','price']
+
